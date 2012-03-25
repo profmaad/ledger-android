4ab4a92f9f7df378f56e48d223e2f8a7ea52263d
profmaad <prof.maad@lambda-bb.de>
2012-03-26 00:32:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ledger-android - An Android client to ledger-rest
+
+ledger-android is an android client to access the functionality of ledger-rest.
+Its main purpose is to quickly add transactions on the go, e.g. when paying cash at a coffee shop.
+
+## What you need
+
+* Android 2.3.3
+* Android SDK for building
---
+++ src/org/profmaad/LedgerAndroid/CreateTransaction.java
@@ -0,0 +1,15 @@
+package org.profmaad.LedgerAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateTransaction extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
